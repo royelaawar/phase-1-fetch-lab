cab9f4c019a1364a6290c007c6268dbaf5bf0098
Roy Elaawar <roy.elaawar@gmail.com>
2023-10-05 16:29:54 -0400

    fetch-lab

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function fetchBooks() {
+
+
+   const gameOfThroneDB = fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(books => renderBooks(books))
+
+  return gameOfThroneDB
+ 
+ 
   // To pass the tests, don't forget to return your fetch!
   
 }
@@ -6,6 +15,7 @@ function fetchBooks() {
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
